a180682651972cab025fae90177baf5ec9565e33
zysndy <zysndy@gmail.com>
2018-10-27 09:39:26 +0800

    chore: update bin

---
+++ index.js
@@ -0,0 +1,9 @@
+const request = require('request')
+(async () => {
+    try {
+        // const res = await request.get('http://dict.youdao.com/w/hello')
+        console.log(res)
+    } catch (e) {
+        console.log(e)
+    }
+})()
